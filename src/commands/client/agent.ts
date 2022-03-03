5d920f799cc31848290e8cbb657af0e99a88ebbe
fayeed <fayeed@live.com>
2022-03-03 15:46:26 +0530

    Added a param to get param

--- src/commands/client/agent.ts
+++ src/commands/client/agent.ts
@@ -5,7 +5,10 @@ import { buildAd4mClient, readPassphrase, prettify, CommonOptions } from './util
 export const command: string = 'agent [action]';
 export const desc: string = 'Agent related action';
 
-type Options = CommonOptions;
+type Options = CommonOptions  & {
+  passphrase?: string;
+};
+;
 
 export const builder = (yargs: Argv) =>
   yargs
@@ -14,18 +17,20 @@ export const builder = (yargs: Argv) =>
       describe: 'Action that should be executed on the agent',
       choices: ['generate', 'lock', 'unlock', 'status', 'me'],
       default: 'status',
+    }).options({
+      passphrase: {type: 'string', describe: 'Password for the agent'}
     });
 
 export const handler = async (argv: Arguments<Options>): Promise<void> => {
-  const { server, verbose, action } = argv;
+  const { server, verbose, action, passphrase } = argv;
   if (verbose) {
     console.info(`Attempting to connect to ${server}`);
   }
   const ad4mClient = buildAd4mClient(server);
   switch (action) {
-    case 'generate': await generate(ad4mClient); break;
+    case 'generate': await generate(ad4mClient, passphrase); break;
     case 'lock': await lock(ad4mClient); break;
-    case 'unlock': await unlock(ad4mClient); break;
+    case 'unlock': await unlock(ad4mClient, passphrase); break;
     case 'status': await status(ad4mClient); break;
     case 'me': await me(ad4mClient); break;
 
@@ -37,8 +42,8 @@ export const handler = async (argv: Arguments<Options>): Promise<void> => {
   process.exit();
 };
 
-async function generate(ad4mClient: Ad4mClient) {
-  const passphrase = readPassphrase();
+async function generate(ad4mClient: Ad4mClient, password: string) {
+  const passphrase = password || readPassphrase();
   const agentStatus = await ad4mClient.agent.generate(passphrase);
   prettify(agentStatus);
 }
@@ -49,8 +54,8 @@ async function lock(ad4mClient: Ad4mClient) {
   prettify(agentStatus);
 }
 
-async function unlock(ad4mClient: Ad4mClient) {
-  const passphrase = readPassphrase();
+async function unlock(ad4mClient: Ad4mClient, password: string) {
+  const passphrase = password || readPassphrase();
   const agentStatus = await ad4mClient.agent.unlock(passphrase);
   prettify(agentStatus);
 }
