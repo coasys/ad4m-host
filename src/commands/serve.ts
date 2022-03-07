f311f95fff19c46a6645183c48f98073e02958f9
fayeed <fayeed@live.com>
2022-03-07 14:57:31 +0530

    Added a way to pass in custom appDefaultLangPath

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -44,19 +44,26 @@ export const builder = (yargs: Argv) =>
         describe: 'The relative path for storing ad4m data', 
         alias: 'rp'
       },
+      appDefaultLangPath: {
+        type: 'string',
+        describe: 'Location of the custom default languages',
+        alias: 'dlp'
+      }
     });
 
 export const handler = async (argv: Arguments<Options>): Promise<void> => {
-  const { port, hcAdminPort, hcAppPort, connectHolochain, dataPath } = argv;
+  const { port, hcAdminPort, hcAppPort, connectHolochain, dataPath, appDefaultLangPath } = argv;
 
   const binaryPath = path.join(getAppDataPath(dataPath || 'ad4m'), 'binary');
+
+  const appDefaultLangLocation: string = path.join(__dirname, appDefaultLangPath as string || "../../temp/languages");
 
   const gqlPort = await getPort({ port })
 
   const config = {
     appDataPath: getAppDataPath(dataPath || ''),
     resourcePath: binaryPath,
-    appDefaultLangPath: path.join(__dirname, "../../temp/languages"),
+    appDefaultLangPath: appDefaultLangLocation,
     ad4mBootstrapLanguages: {
       agents: "agent-expression-store",
       languages: "languages",
@@ -67,7 +74,7 @@ export const handler = async (argv: Arguments<Options>): Promise<void> => {
         {
           address: 'QmR1dV5KuAQtYG98qqmYEvHXfxJZ3jKyjf7SFMriCMfHVQ',
           meta:  {"author":"did:key:zQ3shkkuZLvqeFgHdgZgFMUx8VGkgVWsLA83w2oekhZxoCW2n","timestamp":"2021-10-07T21:39:36.607Z","data":{"name":"Direct Message Language","address":"QmR1dV5KuAQtYG98qqmYEvHXfxJZ3jKyjf7SFMriCMfHVQ","description":"Template source for personal, per-agent DM languages. Holochain based.","possibleTemplateParams":["recipient_did","recipient_hc_agent_pubkey"],"sourceCodeLink":"https://github.com/perspect3vism/direct-message-language"},"proof":{"signature":"e933e34f88694816ea91361605c8c2553ceeb96e847f8c73b75477cc7d9bacaf11eae34e38c2e3f474897f59d20f5843d6f1d2c493b13552093bc16472b0ac33","key":"#zQ3shkkuZLvqeFgHdgZgFMUx8VGkgVWsLA83w2oekhZxoCW2n","valid":true}},
-          bundle: fs.readFileSync(path.join(path.join(__dirname, "../../temp/languages"), 'direct-message-language', 'build', 'bundle.js')).toString()
+          bundle: fs.readFileSync(path.join(appDefaultLangLocation, 'direct-message-language', 'build', 'bundle.js')).toString()
         }
       ],
       perspectives: [],
