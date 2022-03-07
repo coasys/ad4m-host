fd618e30123ef70e214c81074c4811d2e7068bb5
Kaicho Sun <kaichaosuna@gmail.com>
2022-03-07 16:21:04 +0800

    update holochain

--- src/commands/serve.ts
+++ src/commands/serve.ts
@@ -65,10 +65,32 @@ export const handler = async (argv: Arguments<Options>): Promise<void> => {
     ad4mBootstrapFixtures: {
       languages: [
         {
-          address: 'QmR1dV5KuAQtYG98qqmYEvHXfxJZ3jKyjf7SFMriCMfHVQ',
-          meta:  {"author":"did:key:zQ3shkkuZLvqeFgHdgZgFMUx8VGkgVWsLA83w2oekhZxoCW2n","timestamp":"2021-10-07T21:39:36.607Z","data":{"name":"Direct Message Language","address":"QmR1dV5KuAQtYG98qqmYEvHXfxJZ3jKyjf7SFMriCMfHVQ","description":"Template source for personal, per-agent DM languages. Holochain based.","possibleTemplateParams":["recipient_did","recipient_hc_agent_pubkey"],"sourceCodeLink":"https://github.com/perspect3vism/direct-message-language"},"proof":{"signature":"e933e34f88694816ea91361605c8c2553ceeb96e847f8c73b75477cc7d9bacaf11eae34e38c2e3f474897f59d20f5843d6f1d2c493b13552093bc16472b0ac33","key":"#zQ3shkkuZLvqeFgHdgZgFMUx8VGkgVWsLA83w2oekhZxoCW2n","valid":true}},
-          bundle: fs.readFileSync(path.join(path.join(__dirname, "../../temp/languages"), 'direct-message-language', 'build', 'bundle.js')).toString()
-        }
+          address: "QmRENn31FvsZZx99tg8nd8oM52MmGYa1tLUYaDvYdjnJsb",
+          meta: {
+            author:
+              "did:key:zQ3shkkuZLvqeFgHdgZgFMUx8VGkgVWsLA83w2oekhZxoCW2n",
+            timestamp: "2021-10-07T21:39:36.607Z",
+            data: {
+              name: "Direct Message Language",
+              address: "QmRENn31FvsZZx99tg8nd8oM52MmGYa1tLUYaDvYdjnJsb",
+              description:
+                "Template source for personal, per-agent DM languages. Holochain based.",
+              possibleTemplateParams: [
+                "recipient_did",
+                "recipient_hc_agent_pubkey",
+              ],
+              sourceCodeLink:
+                "https://github.com/perspect3vism/direct-message-language",
+            },
+            proof: {
+              signature:
+                "e933e34f88694816ea91361605c8c2553ceeb96e847f8c73b75477cc7d9bacaf11eae34e38c2e3f474897f59d20f5843d6f1d2c493b13552093bc16472b0ac33",
+              key: "#zQ3shkkuZLvqeFgHdgZgFMUx8VGkgVWsLA83w2oekhZxoCW2n",
+              valid: true,
+            },
+          },
+          bundle: fs.readFileSync(path.join(path.join(__dirname, "../../temp/languages"), 'direct-message-language', 'build', 'bundle.js')).toString(),
+        },
       ],
       perspectives: [],
     },
