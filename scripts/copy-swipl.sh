96db74772fb7f82cd6ce68b1fa12d8b714ab9c54
Nicolas Luck <nicolas@lucksus.eu>
2022-07-07 14:16:36 +0200

    Replace symlinks with target in swipl copy

--- scripts/copy-swipl.sh
+++ scripts/copy-swipl.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
-[ -f "./temp/binary/swipl" ] && rm -f ./temp/binary/swipl
+[ -f "./temp/binary/swipl" ] && rm -rf ./temp/swipl
 
 SWIPL_BIN=`which swipl`
 SWIPL_BIN_DIR=`dirname $SWIPL_BIN`
 SWIPL_MAIN_DIR=`dirname $SWIPL_BIN_DIR`
 
-cp $SWIPL_BIN ./temp/binary/ 
-echo "Copied swipl binary to temp/binary"
-
-cp -r $SWIPL_MAIN_DIR/lib ./temp
+cp -r $SWIPL_MAIN_DIR ./temp/swipl/
 echo "Copied swipl lib dir to temp"
 
-#cp -r $SWIPL_MAIN_DIR/share ./temp
-#echo "Copied swipl share dir to temp"
+chmod -R +w temp/swipl
+
+echo "Replacing all symlinks with their target"
+cd ./temp/swipl
+find -type l -exec sh -c 'PREV=$(realpath -- "$1") && rm -- "$1" && cp -ar -- "$PREV" "$1"' resolver {} \;
+
+echo "Copying swipl done."
+
