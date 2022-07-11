8a885512a5b57f39430c13740e1410e7d39572e1
Nicolas Luck <nicolas@lucksus.eu>
2022-07-11 14:56:51 +0200

    Update src/ad4mDataDirectory.ts
    Co-authored-by: Fayeed Pawaskar <fayeed@live.com>

--- src/ad4mDataDirectory.ts
+++ src/ad4mDataDirectory.ts
@@ -1,7 +1,7 @@
 import { homedir } from 'os'
 import path from 'path'
 
-export function ad4mDataDirectory(override = undefined) {
+export function ad4mDataDirectory(override?: string) {
     if(override)
         return path.join(homedir(), override)
     else
